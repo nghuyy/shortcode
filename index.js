764760e54b64db287d61da4cb533387182b57991
Huy Nguyen <huyndx@outlook.com>
2021-01-05 10:30:58 +0700

    Update v 4

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const clipboardy = require('clipboardy');
 require('crypto').randomBytes(128, function(err, buffer) {
     const token = buffer.toString('hex');
     clipboardy.write(token);
+    console.log(token);
 });
